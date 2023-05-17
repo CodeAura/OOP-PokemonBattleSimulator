787fe5392d2c6a56bb1fd626c73c739c76bb88eb
ValidCoder <mikedh.prive@gmail.com>
2023-05-17 10:36:16 +0200

    Basic Formatting for whole project

---
+++ Charmander/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Charmander
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon charmander = new Pokemon();
+            charmander.setName("NeukerBeuker5000");
+            charmander.setAttack("Fire");
+            charmander.setWeakness("Water");
+
+            while (true) {
+                Console.WriteLine("Enter your Charmander Name: ");
+                string? pokemonName = Console.ReadLine();
+
+
+                Console.WriteLine("Pokemon Name: " + charmander.getName());
+                Console.WriteLine("Pokemon Type: " + charmander);
+                Console.WriteLine("Pokemon Type Attack: " + charmander.getTypeAttack());
+                Console.WriteLine("Pokemon Weakness: " + charmander.getWeakness());
+                
+            }
+        }
+    }
+
+    class Pokemon {
+        public String? name;
+        public String? typeAttack;
+        public String? weakness;
+
+        public void setName(String name) { 
+            this.name = name;
+        }
+        public void setAttack(String typeAttack) {
+            this.typeAttack = typeAttack;
+        }
+        public void setWeakness(String weakness) {
+            this.weakness = weakness;
+        }
+        public String? getName() {
+            return name;
+        }
+
+        public String? getTypeAttack() {
+            return typeAttack;
+        }
+
+        public String? getWeakness() {
+            return weakness;
+        }
+    }
+}
+
